7b7f54c10ed9e72afd9e0eeed87d5ac41423d0cc
Miroslav <mir19972905@mai.ru>
2022-06-23 21:31:33 +0300

    create homework task 2

--- 23.06/Program.cs
+++ 23.06/Program.cs
@@ -1,4 +1,32 @@
-﻿Console.WriteLine("Введите число");
-int numberA = Convert.ToInt32(Console.ReadLine());
-int summ = numberA*numberA;
-Console.WriteLine(summ);
+﻿int num1 = 5;
+int num2 = 7;
+if(num1 > num2)
+{
+    Console.WriteLine($"Число {num1} больше числа {num2}");
+}
+else 
+{
+    Console.WriteLine($"Число {num1} меньше числа {num2}");
+}
+
+int num3 = 2;
+int num4 = 10;
+if(num1 > num2)
+{
+    Console.WriteLine($"Число {num3} больше числа {num4}");
+}
+else 
+{
+    Console.WriteLine($"Число {num3} меньше числа {num4}");
+}
+
+int num5 = -9;
+int num6 = -3;
+if(num5 > num6)
+{
+    Console.WriteLine($"Число {num5} больше числа {num6}");
+}
+else 
+{
+    Console.WriteLine($"Число {num5} меньше числа {num6}");
+}
